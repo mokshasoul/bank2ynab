6695bde2341e2394355aae10ffe6183cc8e86974
Charis-Nicolas Georgiou <cng_it@posteo.net>
2023-08-15 20:03:40 +0200

    feat: mnext sage

--- bank2ynab/dataframe_handler.py
+++ bank2ynab/dataframe_handler.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import logging
-from typing import Dict, List
 
 import pandas as pd
 
--- bank2ynab/ynab/client.py
+++ bank2ynab/ynab/client.py
@@ -15,11 +15,11 @@ class YNABClient:
 
         self.auth_token = api_token
         self.client = requests.session()
-        self.base_ur = f"https://{self.API_ENDPOINT}/{self.API_VERSION}"
+        self.base_uri = f"https://{self.API_ENDPOINT}/{self.API_VERSION}"
         self.client.headers.update({"Authorization": f"Bearer {api_token}"})
 
     def list_budgets(self) -> dict:
-        request_uri = f"{self.base_ur}/budget"
+        request_uri = f"{self.base_uri}/budget"
         res = self.client.get(url=request_uri)
 
         if res.status_code >= 300:
@@ -29,7 +29,7 @@ class YNABClient:
         return res.json()["data"]
 
     def get_budget(self, budget_id: str) -> dict:
-        request_uri = f"{self.base_ur}/budget/{budget_id}"
+        request_uri = f"{self.base_uri}/budget/{budget_id}"
         res = self.client.get(url=request_uri)
         if res.status_code >= 300:
             # TODO: raise error
@@ -38,7 +38,7 @@ class YNABClient:
         return res.json()["data"]
 
     def list_accounts(self, budget_id: str) -> dict:
-        request_uri = f"{self.base_ur}/budget/{budget_id}/accounts"
+        request_uri = f"{self.base_uri}/budget/{budget_id}/accounts"
         res = self.client.get(url=request_uri)
         if res.status_code >= 300:
             # TODO: raise error
@@ -47,7 +47,7 @@ class YNABClient:
         return res.json()["data"]
 
     def get_account(self, budget_id: str, account_id: str) -> dict:
-        request_uri = f"{self.base_ur}/budget/{budget_id}/accounts/{account_id}"
+        request_uri = f"{self.base_uri}/budget/{budget_id}/accounts/{account_id}"
         res = self.client.get(url=request_uri)
         if res.status_code >= 300:
             # TODO: raise error
