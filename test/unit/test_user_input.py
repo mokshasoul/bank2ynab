835ed67134ebb5b6484782aa170101db19606b07
Charis-Nicolas Georgiou <cng_it@posteo.net>
2023-09-19 23:45:37 +0200

    refactor: update failing test

--- test/unit/test_user_input.py
+++ test/unit/test_user_input.py
@@ -5,12 +5,6 @@ from bank2ynab.user_input import display_options, get_int_input, get_user_input
 
 
 class TestUserInput(TestCase):
-    def setUp(self) -> None:
-        return super().setUp()
-
-    def tearDown(self) -> None:
-        return super().tearDown()
-
     @patch("builtins.input")
     def test_get_user_input(self, mock_input):
         """Test correct return of option string from user input."""
@@ -27,8 +21,8 @@ class TestUserInput(TestCase):
                 return_string = get_user_input(test_options, test_message)
                 self.assertEqual(test_options[test_input - 1][1], return_string)
 
-    @patch("builtins.print")
-    def test_option_display(self, mock_print):
+    @patch("bank2ynab.user_input.logger")
+    def test_option_display(self, mock_log):
         """Test correct list display of options."""
         test_options = [
             ["Bank 1", "ID 1"],
@@ -37,12 +31,12 @@ class TestUserInput(TestCase):
         ]
         display_options(test_options)
         calls = [
-            call("\n"),
-            call("| 1 | Bank 1"),
-            call("| 2 | Bank 2"),
-            call("| 3 | Bank 3"),
+            call.info("\n"),
+            call.info("| %s | %s", 1, "Bank 1"),
+            call.info("| %s | %s", 2, "Bank 2"),
+            call.info("| %s | %s", 3, "Bank 3"),
         ]
-        mock_print.assert_has_calls(calls, any_order=False)
+        mock_log.assert_has_calls(calls, any_order=False)
 
     @patch("builtins.input")
     def test_get_int_input(self, mock_input):
